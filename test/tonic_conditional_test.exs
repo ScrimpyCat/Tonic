5892a9a86dc85fdc5ef485f68d585ceaebe4a5dc
Stefan Johnson <ScrimpyCat@gmail.com>
2014-11-18 17:42:31 +1100

    Conditional tests

---
+++ test/tonic_conditional_test.exs
@@ -0,0 +1,58 @@
+defmodule TonicConitionalTests do
+    defmodule SimpleOn do
+        use ExUnit.Case
+        use Tonic
+
+        uint8 :type
+        on get(:type) do
+            1 -> uint32
+            2 -> float32
+        end
+        
+        test "simple on statement" do
+            assert { { { :type, 2 }, 2.5 }, <<>> } == Tonic.load(<<2, 2.5 :: float-size(32)-native>>, __MODULE__)
+        end
+    end
+
+    defmodule ScopeOn do
+        use ExUnit.Case
+        use Tonic
+
+        endian :little
+        uint32 :type
+        on get(:type) do
+            1 -> endian :little
+            2 -> endian :big
+        end
+        float32
+        
+        test "correct scoping for on statement" do
+            assert { { { :type, 2 }, 2.5 }, <<>> } == Tonic.load(<<2 :: integer-size(32)-little, 2.5 :: float-size(32)-big>>, __MODULE__)
+        end
+    end
+
+    defmodule MultilineOn do
+        use ExUnit.Case
+        use Tonic
+
+        uint8 :type, fn { _, v } -> v end
+        on get(:type) do
+            1 -> 
+                uint32
+                uint32
+                group do
+                    float32
+                end
+            2 -> float32
+        end
+        
+        test "multiline statement" do
+            assert { { 1, 15, 123, { 2.5 } }, <<>> } == Tonic.load(<<
+                1, 
+                    15 :: integer-size(32)-native,
+                    123 :: integer-size(32)-native,
+                    2.5 :: float-size(32)-native
+            >>, __MODULE__)
+        end
+    end
+end
