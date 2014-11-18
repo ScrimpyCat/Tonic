1a59c61140369f227db1935a082dc45d9f17cedb
Stefan Johnson <ScrimpyCat@gmail.com>
2014-11-18 15:06:15 +1100

    Unnamed string tests

--- test/tonic_string_test.exs
+++ test/tonic_string_test.exs
@@ -4,7 +4,7 @@ defmodule TonicStringTests do
         use Tonic
 
         string :name
-        
+
         test "string with no options" do
             assert { { { :name, "12345678" } }, <<>> } == Tonic.load(<<"12345678">>, __MODULE__)
         end
@@ -15,7 +15,7 @@ defmodule TonicStringTests do
         use Tonic
 
         string :name, length: 4
-        
+
         test "string with length" do
             assert { { { :name, "1234" } }, <<"5678">> } == Tonic.load(<<"12345678">>, __MODULE__)
         end
@@ -38,9 +38,54 @@ defmodule TonicStringTests do
 
         string :name, length: 5, terminator: ?\n
         string :name, length: 5, terminator: ?\n
-        
+
         test "string with length" do
             assert { { { :name, "1234" }, { :name, "56789" } }, <<"\n">> } == Tonic.load(<<"1234\n56789\n">>, __MODULE__)
+        end
+    end
+
+    defmodule UnnamedStringTillEnd do
+        use ExUnit.Case
+        use Tonic
+
+        string
+
+        test "unnamed string with no options" do
+            assert { { "12345678" }, <<>> } == Tonic.load(<<"12345678">>, __MODULE__)
+        end
+    end
+
+    defmodule UnnamedStringLength do
+        use ExUnit.Case
+        use Tonic
+
+        string length: 4
+
+        test "unnamed string with length" do
+            assert { { "1234" }, <<"5678">> } == Tonic.load(<<"12345678">>, __MODULE__)
+        end
+    end
+
+    defmodule UnnamedStringNullTerminator do
+        use ExUnit.Case
+        use Tonic
+
+        string 0
+
+        test "unnamed string with null terminator" do
+            assert { { "1234" }, <<"5678">> } == Tonic.load(<<"1234", 0, "5678">>, __MODULE__)
+        end
+    end
+
+    defmodule UnnamedStringLengthWithNullTerminator do
+        use ExUnit.Case
+        use Tonic
+
+        string length: 5, terminator: ?\n
+        string length: 5, terminator: ?\n
+
+        test "unnamed string with length" do
+            assert { { "1234", "56789" }, <<"\n">> } == Tonic.load(<<"1234\n56789\n">>, __MODULE__)
         end
     end
 end
